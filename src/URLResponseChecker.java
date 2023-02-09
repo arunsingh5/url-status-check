81690e7f8f37c5ae48a964c4839b549741fd510f
Singh <arunkumar.singh@bt.com>
2023-02-09 22:43:23 +0530

    initial commit

---
+++ src/URLResponseChecker.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * This program take sitemap URL as input and prints http response of URLs
+ * 
+ * @author Arun Kumar Singh
+ *
+ */
+public class URLResponseChecker {
+	public static void main(String[] args) {
+		//String sitemap = "https://www.globalservices.bt.com/en/sitemap.xml";
+		String sitemap = args[0];
+		if(null == sitemap) {
+			System.out.println("Please enter sitemap URL as argument.");
+			return;
+		}
+		try {
+			URL url = new URL(sitemap);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				int startIndex = 0;
+				if (line.contains("<loc>")) {
+					int counter = 0;
+					while (startIndex != -1) {
+						startIndex = line.indexOf("<loc>", counter);
+						int endIndex = line.indexOf("</loc>", startIndex);
+
+						String urlStr = line.substring(startIndex + 5, endIndex);
+						counter = endIndex + 6;
+
+						URL pageURL = new URL(urlStr);
+						HttpURLConnection connection = (HttpURLConnection) pageURL.openConnection();
+						connection.setRequestMethod("GET");
+						int responseCode = connection.getResponseCode();
+						if (responseCode == 200) {
+							System.out.println(urlStr + " --> " + responseCode);
+						} else {
+							System.out.println("******************* Error Page **************************");
+							System.out.println(urlStr + " --> " + responseCode);
+							System.out.println("**********************************************************");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("An error occurred while checking the URLs: " + e.getMessage());
+		}
+	}
+}
